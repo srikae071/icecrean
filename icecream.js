c7cfc2259d6670f27622d4ab75da5cc4ae351c01
srikar <62554056+srikae071@users.noreply.github.com>
2023-03-21 11:12:56 +0530

    Add files via upload

---
+++ icecream.js
@@ -0,0 +1,30 @@
+document.getElementById("sampleid").addEventListener("mouseover", function () {
+  document.getElementById("sampleid").style.color = "#d16276";
+});
+document.getElementById("sampleid").addEventListener("mouseout", function () {
+  document.getElementById("sampleid").style.color = "";
+});
+document.getElementById("homei").addEventListener("mouseover", function () {
+  document.getElementById("homei").style.color = "#d16276";
+});
+document.getElementById("homei").addEventListener("mouseout", function () {
+  document.getElementById("homei").style.color = "";
+});
+document
+  .getElementById("Testimonialsi")
+  .addEventListener("mouseover", function () {
+    document.getElementById("Testimonialsi").style.color = "#d16276";
+  });
+document
+  .getElementById("Testimonialsi")
+  .addEventListener("mouseout", function () {
+    document.getElementById("Testimonialsi").style.color = "";
+  });
+document
+  .getElementById("contactusi")
+  .addEventListener("mouseover", function () {
+    document.getElementById("contactusi").style.color = "#d16276";
+  });
+document.getElementById("contactusi").addEventListener("mouseout", function () {
+  document.getElementById("contactusi").style.color = "";
+});
